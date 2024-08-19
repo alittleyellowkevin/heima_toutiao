e7f0de10b31a795e94ba49111be64badf369c2ac
kevin <1648053382@qq.com>
2024-08-19 14:49:21 +0800

    day3

--- heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/controller/ApUserLoginController.java
+++ heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/controller/ApUserLoginController.java
@@ -5,6 +5,7 @@ import com.heima.model.user.dtos.LoginDto;
 import com.heima.user.service.ApUserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
---
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/config/WebMvcConfiguration.java
@@ -0,0 +1,31 @@
+package com.heima.wemedia.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.heima.wemedia.interceptor.WmTokenIntercaptor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * 配置类，注册web层相关组件
+ */
+@Configuration
+@Slf4j
+public class WebMvcConfiguration extends WebMvcConfigurationSupport {
+    /**
+     * 注册自定义拦截器
+     *
+     * @param registry
+     */
+    protected void addInterceptors(InterceptorRegistry registry) {
+        log.info("开始注册自定义拦截器...");
+        registry.addInterceptor(new WmTokenIntercaptor())
+                .addPathPatterns("/**");
+    }
+
+}
