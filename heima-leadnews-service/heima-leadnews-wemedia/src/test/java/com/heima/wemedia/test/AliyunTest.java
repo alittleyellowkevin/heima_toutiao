6da3caca4a81649d741eb30240fb2195501b6038
kevin <1648053382@qq.com>
2024-08-19 20:06:33 +0800

    day4

---
+++ heima-leadnews-service/heima-leadnews-wemedia/src/test/java/com/heima/wemedia/test/AliyunTest.java
@@ -0,0 +1,45 @@
+package com.heima.wemedia.test;
+
+
+import com.heima.common.aliyun.GreenImageScan;
+import com.heima.common.aliyun.GreenTextScan;
+import com.heima.file.service.FileStorageService;
+import com.heima.wemedia.WemediaApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Map;
+
+@SpringBootTest(classes = WemediaApplication.class)
+@RunWith(SpringRunner.class)
+public class AliyunTest {
+
+
+    @Autowired
+    private GreenTextScan greenTextScan;
+
+    @Autowired
+    private GreenImageScan greenImageScan;
+
+    @Autowired
+    private FileStorageService fileStorageService;
+    /**
+     * 测试文本内容审核
+     */
+    @Test
+    public void testScanText() throws Exception {
+        Map map = greenTextScan.greeTextScan("我是一个好人,冰毒");
+        System.out.println(map);
+    }
+
+    /**
+     * 测试图片审核
+     */
+    @Test
+    public void testScanImage(){
+
+    }
+}
